7e3a540e5efcf9b07797e546e75409d2bb080845
zaenk <peter.pribula@hotmail.com>
2019-09-21 19:09:28 +0200

    Adds code that fails in Chrome 77, but works in 76, 78, 79, FF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as openpgp from 'openpgp';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chrome77-zonejs-promise-bug';
+
+  constructor() {
+    console.log(navigator.userAgent);
+    const options = {
+      userIds: [{ name:'Jon Smith', email:'jon@example.com' }],
+      rsaBits: 4096,
+      passphrase: 'super long and hard to guess secret'
+    };
+    openpgp.generateKey(options).then(key => {
+      const privkey = key.privateKeyArmored;
+      return privkey;
+    }).then(armoredPrivKey => {
+      console.log('devtools needed a block for breakpoints');
+      return openpgp.key.readArmored(armoredPrivKey);
+    }).then(privKeyChain => {
+      console.log('in chrome 77 this point is never reached, because it throws Method Promise.prototype.then called on incompatible receiver [object Object]');
+      console.log(privKeyChain.keys[0].getKeyId());
+    });
+  }
 }
